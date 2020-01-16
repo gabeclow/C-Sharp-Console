a36c73e4749f1b9dc95d3255e11f66ec9c04894e
Gabe Clow <Gabe.jclow@gmail.com>
2020-01-16 09:15:56 -0800

    Separatation of classes from Program
    classes will now have their own file, program will only contain MAIN

---
+++ C-Sharp Console/Delegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Console
+{
+    class Delegate
+    {
+        public delegate bool delegateFilter(int i);
+        static bool lessThanFive(int n) { return n < 5; }
+        static bool lessThanTen(int n) { return n < 10; }
+        static bool greaterThanTen(int n) { return n > 10; }
+
+        //#This number-filter takes a delegate as well as a list of numbers
+        static IEnumerable<int> NumberFilter(IEnumerable<int> numbers, delegateFilter filter)
+        {
+            foreach (int num in numbers)
+            {
+                if (filter(num)) //#This line calls the delegate 
+                {
+                    yield return num;
+                }
+            }
+        }
+
+        public void RunDelegateProgram()
+        {
+            int[] numbers = new[] { 3, 3, 5, 6, 8, 2, 231, 6, 8, 8, 4, 2, 1, 8, 89 };
+
+            //#will make a list from numbers which suffice the filter
+            IEnumerable<int> result = NumberFilter(numbers, lessThanFive);
+
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+    }
+}
+
+
+
