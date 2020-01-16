ef9db5ea848caf14409c61c93bcefa56028f334a
Gabe Clow <Gabe.jclow@gmail.com>
2020-01-16 14:24:02 -0800

    added Factory Pattern

--- C-Sharp Console/Program.cs
+++ C-Sharp Console/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 
 namespace C_Sharp_Console
 {
@@ -11,11 +11,21 @@ namespace C_Sharp_Console
 
         static void Main(string[] args)
         {
-
-
-            //Delegate delegateExampleRun = new Delegate();
-            //delegateExampleRun.RunDelegateProgram();
-            Console.ReadLine();
+            Document[] documents = new Document[3];
+            documents[0] = new Resume();
+            documents[1] = new Report();
+            documents[2] = new BluePrint();
+            // Display document pages
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+            Console.Read();
         }
+
     }
 }
--- C-Sharp Console/Threading.cs
+++ C-Sharp Console/Threading.cs
@@ -1,12 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace C_Sharp_Console
 {
     class Threading
     {
+        public delegate void CallTwo();
+
+        private void delegateCallThreadTwo(CallTwo callTwo)
+        {
+            callTwo();
+        }
+
+        public void SimilarCall()
+        {
+            delegateCallThreadTwo(ThreadTwoFact);
+            delegateCallThreadTwo(ThreadTwoFactPlusTen);
+            delegateCallThreadTwo(ThreadThree);
+            delegateCallThreadTwo(ThreadThree2);
+        }
+
+        //#calculates factorial of a number via recursion
+        private double ThreadOneFactorial(int n)
+        {
+            double result = n;
+            if (n <= 1)
+            {
+                return n;
+            }
+            return result *= ThreadOneFactorial(n - 1);
+        }
+
+        private static object syncLock = new object();
+
+        private void ThreadTwoFact()
+        {
+            lock (syncLock)
+            {
+                double factorialResult = ThreadOneFactorial(5);
+                Console.WriteLine(factorialResult);
+            }
+        }
+        
+        private void ThreadTwoFactPlusTen()
+        {
+            lock (syncLock)
+            {
+                double factorialResult = ThreadOneFactorial(5) + 10;
+                Console.WriteLine(factorialResult);
+            }
+        }
+
+        private void ThreadThree()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                int j = i % 10;
+                Console.WriteLine(i);
+            }
+        }
+        private void ThreadThree2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int j = i % 10;
+                Console.WriteLine(i);
+            }
+        }
+
     }
 }
