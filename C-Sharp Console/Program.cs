57487f9059fb8878d741715b5afd95ead1e21f02
Gabe Clow <Gabejclow@gmail.com>
2020-01-15 17:09:25 -0800

    delegate filters

--- C-Sharp Console/Program.cs
+++ C-Sharp Console/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace C_Sharp_Console
 {
     class Program
     {
+        public delegate bool delegateFilter(int i);
+        static bool lessThanfive(int n) { return n < 5; }
+        static bool lessThanten(int n) { return n < 10; }
+        static bool greaterThanTen(int n) { return n > 10; }
+
+
+        static IEnumerable<int> NumberFilter(IEnumerable<int> numbers, delegateFilter filter)
+        {
+            foreach (int num in numbers)
+            {
+                if (filter(num))
+                {
+                    yield return num;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            int[] numbers = new[] { 3, 3, 5, 6, 8, 2, 231, 6, 8, 8, 4, 2, 1, 8, 89 };
+            IEnumerable<int> result = NumberFilter(numbers, lessThanFive);
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }                
+            Console.ReadLine();
         }
     }
 }
